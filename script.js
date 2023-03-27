eb14ffc079b14872e34e97fbdc3c016afd4113f8
David <liid05754@gmail.com>
2023-03-28 00:21:30 +0300

    commit

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let button = document.querySelector('.button');
 let modal = document.querySelector('.modal');
 
 modal__close.onclick = function() {
-    modal.classList.toggle('active');
-};
-
-button.onclick = function() {
     modal.classList.remove('active');
 };
 
+button.onclick = function() {
+    modal.classList.add('active');
+};
+
 modal__overlay.onclick = function() {
-    modal.classList.toggle('active');
+    modal.classList.remove('active');
 };
 
